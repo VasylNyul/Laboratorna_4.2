b185774af2ecd5d49b58667d34879b926c9e346e
Nyul <Vasyl.Niul.IT.2021@lpnu.ua>
2021-10-05 16:59:00 +0300

    laba_4_2

---
+++ Laba_4_2.hpp
@@ -0,0 +1,13 @@
+//
+//  Laba_4_2.hpp
+//  Laboratorna_4.2
+//
+//  Created by apple on 04.10.2021.
+//
+
+#ifndef Laba_4_2_hpp
+#define Laba_4_2_hpp
+
+#include <stdio.h>
+
+#endif /* Laba_4_2_hpp */
